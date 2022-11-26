6b8d131cd77a4babe6f3a5f05662361c27098c27
Yinon Bar <yinonbar1988@gmail.com>
2022-11-26 17:26:07 +0200

    clean slate

---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import "./Content.css";
+
+function Content() {
+  return (
+    <div className="Content">
+      <div className="Container">
+        <h2>Content</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      Copyright &copy; 2021 Yinon Bar | Web Developer
+    </div>
+  );
+}
